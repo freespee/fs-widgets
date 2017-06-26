7bd6364039e5d348ee3f2dcb85b928b464187da5
Damoon Rashidi <damoon.rashidi@freespee.com>
2017-06-26 14:52:00 +0200

    fixed problem for elias

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/angularjs/angular.d.ts" />
+import * as angular from 'angular';
 
 export const Component = function(options: ng.IComponentOptions): Function {
   return (controller: Function) => {
