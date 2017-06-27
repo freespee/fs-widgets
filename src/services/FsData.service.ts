e796b97004917840b1cba03ffd3b7b74b0e6d886
Elias Chaaya <chaaya@Eliass-MacBook-Pro.local>
2017-06-27 09:45:56 +0200

    changed to a provider

--- src/services/FsData.service.ts
+++ src/services/FsData.service.ts
@@ -3,12 +3,36 @@ interface Datasource {
   name: string;
 }
 
-class FsData {
-  subcustomerId: number;
-  customerId: number;
+class FsData implements ng.IServiceProvider {
+  _subcustomerId: number;
+  _customerId: number;
+  _baseUrl: string = 'https://analytics.freespee.com/be/';
 
   constructor(private $http: ng.IHttpService, private $q: ng.IQService) {
 
+  }
+
+  set subcustomerId(subcustomerId: number) {
+      this._subcustomerId = subcustomerId;
+  }
+
+  set customerId(customerId: number) {
+      this.customerId = customerId;
+  }
+
+  set baseUrl(baseUrl: string) {
+      this.baseUrl = baseUrl;
+  }
+
+  $get() {
+    return {
+      logSubCust: this.logSubCust,
+      getDatasources: this.getDatasources
+    }
+  }
+
+  logSubCust(): void {
+    console.log('the subcust is now', this._subcustomerId);
   }
 
   getDatasources() : ng.IPromise<Datasource[]> {
