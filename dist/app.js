45229f117c27636dc5addf0251a9453b320f5c19
sagakarlsson <saga.karlsson@freespee.com>
2017-09-08 12:12:34 +0200

    Added support for missed_calls widgets.

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,9 @@ let app = angular.module('app', ['fs-widgets']);
 app.config( FsDataProvider => {
   FsDataProvider.customerId = 432657;
   FsDataProvider.partnerId = 3;
-  FsDataProvider.datasourceUrl = '/datasources?partner_id={{partnerId}}&customer_id={{customerId}}';
-  FsDataProvider.dataUrl = '/data?type={{type}}&customer_id={{customerId}}';
-  FsDataProvider.baseUrl = 'http://localhost:3000';
+  FsDataProvider.datasourceUrl = '/api/widgets/datasources?partner_id={{partnerId}}&customer_id={{customerId}}';
+  FsDataProvider.dataUrl = '/api/widgets/content?type={{type}}&customer_id={{customerId}}';
+  FsDataProvider.baseUrl = 'https://analytics.freespee.com';
 });
 
 app.controller('MyCtrl', MyCtrl);
